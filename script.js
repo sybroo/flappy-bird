1f9bfa3017b2925bf7f222a84ae1024d43174bb6
Syb Scholing <74767501+sybroo@users.noreply.github.com>
2021-09-20 09:34:21 +0000

    bal zit in het midden, mar we moesten in het in het midden van onze verandringing dus nu werkt het niet

--- script.js
+++ script.js
@@ -1,60 +1,38 @@
-function setup() {
-	createCanvas(500, 400);
+class Ball {
+    constructor(x,y,w,h,g,vy){
+    this.y = y;
+    this.x = x;
+    this.width = w;
+    this.height = h;
+    this.gravity = g;
+    this.vy = vy;    
+  }
+
+  function Ball() {
+    this.
+  }
+
+  drawBall(){   
+    fill ('lightpink')
+    ellipse(this.x,this.y,this.width,this.height)
+    this.y = this.y + this.vy;
+
+
+    if(this.y <= 0 || this.y >= 400){
+      this.vy = -this.vy
+    }
+  
+  }
 }
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+function setup() {
+  createCanvas(500, 400); 
+
+  ball1 = new Ball(250,200,20,20,0,0);
+}
 
 function draw() {
-	background(225);
-	
-	fill(0);
-	text("Use the arrow keys (or WASD) to move the square around", 25, 25);
-	
-	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
-	
-	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
-}
-
-function keyPressed() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
-	}
-}
-
-function keyReleased() {
-	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
-	}
+  background('black');  
+  
+  ball1.drawBall(); 
 }
