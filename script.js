96ddc4248573ffec26b001cf97a90f782afd76b5
Syb Scholing <74767501+sybroo@users.noreply.github.com>
2021-10-11 10:39:08 +0000

    coalition

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 var gravity = 0.01;
 //let img;
 //function preload() {
-  //img = loadImage('images/pacman.png');
+//img = loadImage('images/pacman.png');
 //}
 class Ball {
-  constructor(x, y, w, h, vy,) {
+  constructor(x, y, w, h, vy, ) {
     this.x = x;
     this.y = y;
     this.width = w;
     this.height = h;
     this.vy = vy;
   }
-  
-  
+
+
   drawBall() {
     fill("yellow");
 
 
     this.vy += gravity;
-    
-    if (this.y < 390) { 
+
+    if (this.y < 390) {
       this.y = this.y + this.vy;
     }
-     ellipse(this.x, this.y, this.width, this.height);
+    rect(this.x, this.y, this.width, this.height);
     //image(img, this.x, this.y, this.width, this.height);
   }
 }
@@ -36,58 +36,68 @@ class Rect {
     this.vx = vx;
     this.color = color
   }
-  
-  drawRect() {
-   fill(this.color)
-   rect(this.x, this.y, this.width, this.height);
-   this.x = this.x +this.vx;
-  } 
 
-  checkCollision(){
-    if(ball1.x > this.x){
-      this.color = "red";
+  drawRect() {
+    fill(this.color)
+    rect(this.x, this.y, this.width, this.height);
+    this.x = this.x + this.vx;
+  }
+
+  checkCollision() {
+    //console.log(ball1.x,this.x, this.width)
+    if (ball1.x + ball1.width > this.x && ball1.x < this.x + this.width)
+    if (ball1.y + ball1.height > this.y && ball1.y < this.y + this.height)
+     {
+      if(1){
+        this.color = "red";
+      }
+      }
+
+    else {
+      this.color = "lightgreen";
     }
   }
 }
 
 
+
 var gravity, ball1;
 
-function keyPressed(spacebar){  
+function keyPressed(spacebar) {
   ball1.vy = ball1.vy - 5;
 }
- 
+
 
 var pipes = [];
 function setup() {
   createCanvas(600, 400);
   gravity = 0.25;
   ball1 = new Ball(250, 200, 20, 20, 0);
-  
+
 }
 
 function draw() {
   background('lightblue');
   ball1.drawBall();
-  
-  if(frameCount % 80 == 0){
+
+  if (frameCount % 80 == 0) {
     let randomHeight = random(height - 100);
 
-    recta = new Rect(800, 0, 40, randomHeight, -3,"lightgreen");
-    rectb = new Rect(800, randomHeight + 100, 40, 600, -3,"lightgreen");
-   
+    recta = new Rect(800, 0, 40, randomHeight, -3, "lightgreen");
+    rectb = new Rect(800, randomHeight + 100, 40, 600, -3, "lightgreen");
+
 
     pipes.push(recta);
     pipes.push(rectb);
 
-    if(pipes.length > 8){
-      pipes.splice(0,2);
+    if (pipes.length > 8) {
+      pipes.splice(0, 2);
     }
   }
 
   pipes.forEach((p) => {
     p.drawRect();
     p.checkCollision();
-  });  
+  });
 }
 
